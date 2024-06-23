1727f2360796eeb93d4f9f3bb2a8a2d81c508a00
Guilherme Modena <guilherme.renato.modena@gmail.com>
2024-06-22 21:50:16 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+        Nokia nokia = new Nokia("1234567890", "Nokia 3310", "123456789012345", 16);
+
+        // Instalar aplicativo no Nokia
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Criar um objeto Iphone
+        Iphone iphone = new Iphone("9876543210", "Iphone 13", "987654321098765", 128);
+
+        // Instalar aplicativo no Iphone
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.ReadLine();
+    
